c7fca6000edcbf4a1990f8998d274c1eb225a209
F-shaikh <38937921+F-shaikh@users.noreply.github.com>
2018-05-03 01:25:17 -0700

    Update ListClient.c

--- ListClient.c
+++ ListClient.c
@@ -1,8 +1,3 @@
-// Faisal Shaikh
-// Fshaikh1@ucsc.edu
-// Student ID: 1463626
-// PA2
-
 /****************************************************************************************
 *  ListClient.c
 *  Test client for List ADT
