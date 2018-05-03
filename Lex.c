4d051f2149e5dfb08d4cba0808e254f43c7e4c2d
F-shaikh <38937921+F-shaikh@users.noreply.github.com>
2018-05-03 01:23:57 -0700

    Add files via upload
    Goal in this project is to build an Integer List ADT that will be used to alphabetize lines in a file.

---
+++ Lex.c
@@ -0,0 +1,98 @@
+// Faisal Shaikh
+// Fshaikh1@unix.ucsc.edu
+// Student ID: 1463626
+// PA2
+
+//-----------------------------------------------------------------------------
+// FileIO.c
+// Illustrates file input-output commands and text processing using the
+// string functions strtok and strcat.
+//-----------------------------------------------------------------------------
+#include "List.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX_LEN 160
+
+int main(int argc, char * argv[])
+{
+    FILE *in, *out;
+    char line[MAX_LEN];
+    int myLines = 0;
+    // check command line for correct number of arguments
+    if (argc != 3)
+    {
+        printf("Usage: %s <input file> <output file>\n", argv[0]);
+        exit(1);
+    }
+
+    // open files for reading and writing
+    in = fopen(argv[1], "r");
+    out = fopen(argv[2], "w");
+    if (in == NULL)
+    {
+        printf("Unable to open file %s for reading\n", argv[1]);
+        exit(1);
+    }
+    if (out == NULL)
+    {
+        printf("Unable to open file %s for writing\n", argv[2]);
+        exit(1);
+    }
+
+    /* read each line of input file, then count and print tokens */
+    while(fgets(line, MAX_LEN, in) != NULL)
+    {
+        myLines++;
+    }
+    rewind(in);
+    char *array[myLines];
+
+    for(int i = 0; i < myLines; i++)
+    {
+        fgets(line,MAX_LEN,in);
+        array[i] = (char*) malloc(sizeof(line));
+        strcpy(array[i],line);
+    }
+    List list = newList();
+    int flag = 1;
+    append(list,0); // Starting point for comparison.
+    for (int i = 1; i < myLines; i++)
+    {
+        moveFront(list);
+        flag = 1;
+        while (index(list) != -1 && flag == 1)
+        {
+            if (strcmp(array[get(list)], array[i]) > 0)
+            {
+                insertBefore(list, i);
+                flag = 0;
+            }
+            else
+            {
+                moveNext(list);
+            }
+        }
+        if (index(list) == -1)
+        {
+            append(list,i);
+        }
+    }
+    moveFront(list);
+    while(index(list) != -1)
+    {
+        fprintf(out, array[get(list)]);
+        moveNext(list);
+    }
+
+    for(int i = 0; i< myLines; i++)
+    {
+        free(array[i]);
+    }
+    freeList(&list);
+    /* close files */
+    fclose(in);
+    fclose(out);
+    return (0);
+}
---
+++ List.c
@@ -0,0 +1,473 @@
+// Faisal Shaikh
+// Fshaikh1@ucsc.edu
+// Student ID: 1463626
+// PA2
+
+#include<stdio.h>
+#include <stdlib.h>
+#include "List.h"
+
+// Structs --------------------------------------------------------------------
+typedef struct NodeObj
+{
+    int data;
+    struct NodeObj* prev;
+    struct NodeObj* next;
+} NodeObj;
+
+// private Node type
+typedef NodeObj* Node;
+
+// Private ListObj type
+typedef struct ListObj
+{
+    int length;
+    int cursorIndex;
+    Node front;
+    Node back;
+    Node cursor;
+}ListObj;
+
+// Constructors-Destructors ---------------------------------------------------
+Node newNode(int data)
+{
+    Node node = malloc(sizeof(NodeObj));
+    node->data = data;
+    node->prev = NULL;
+    node->next = NULL;
+    return(node);
+}
+
+void freeNode(Node *pointerNode)
+{
+    if (pointerNode != NULL && *pointerNode != NULL)
+    {
+        free(*pointerNode);
+        *pointerNode = NULL;
+    }
+}
+List newList(void)
+{ // Creates new empty list
+    List L;
+    L = malloc(sizeof(ListObj));
+    L->cursorIndex = -1;
+    L->length = 0;
+    L->front = NULL;
+    L->back = NULL;
+    L->cursor = NULL;
+    return (L);
+}
+void freeList(List* pointerList)
+{
+    if (pointerList != NULL && *pointerList != NULL)
+    {
+        clear(*pointerList); // Can use clear function to empty the list
+        free(*pointerList);
+        *pointerList = NULL;
+    }
+}
+// Access functions -----------------------------------------------------------
+int length(List L)
+{
+    if (L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+        return (L->length);
+}
+
+int index(List L)
+{
+    if (L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    return (L->cursorIndex);
+}
+
+int front(List L)
+{
+    if (L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    return (L->front->data);
+}
+
+int back(List L)
+{
+    if (L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    return (L->back->data);
+}
+
+int get(List L)
+{
+    if (L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    return L->cursor->data;
+}
+
+int equals(List A, List B)
+{
+    if (A->length == B->length)
+    {
+        if (A->length == 0 && B->length == 0)
+        {
+            return 1;
+        }
+        Node temp = A->front;
+        Node temp2 = B->front;
+        while (temp != NULL && temp != NULL) // Compare both elements starting from the front
+        {
+            if (temp->data != temp2->data)
+            {
+                return 0;
+            }
+            temp = temp->next;   // move to next element of List A
+            temp2 = temp2->next; // move to next element of List B
+        }
+        return 1;
+    }
+    else
+        return 0;
+}
+// Manipulation procedures ----------------------------------------------------
+void clear(List L)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    while (L->back != NULL) // While last element isnt null, just keep deleting the last element to clear list.
+    {
+        deleteBack(L);
+    }
+}
+
+void moveFront(List L)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    if (L != NULL)
+    {
+        if (L->length > 0)
+        {
+            L->cursor = L->front;
+            L->cursorIndex = 0;
+        }
+    }
+}
+void moveBack(List L)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    if (L != NULL)
+    {
+        if (L->length > 0)
+        {
+            L->cursor = L->back;
+            L->cursorIndex = L->length - 1;
+        }
+    }
+}
+void movePrev(List L)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    if (L->cursor != NULL)
+    {
+        if (L->cursor != L->front)
+        {
+            L->cursor = L->cursor->prev;
+            L->cursorIndex--;
+        }
+        else if (L->cursor == L->front) // Adjust the new cursor if cursor happens to be front
+        {
+            L->cursor = NULL;
+            L->cursorIndex = -1;
+        }
+    }
+}
+void moveNext(List L)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    if (L != NULL)
+    {
+        if (L->cursor != NULL)
+        {
+            if (L->cursor != L->back)
+            {
+                L->cursor = L->cursor->next;
+                L->cursorIndex++;
+            }
+            else if (L->cursor->next == NULL) // Adjust the new cursor if cursor happens to be back
+            {
+                L->cursor = NULL;
+                L->cursorIndex = -1;
+            }
+        }
+    }
+}
+void prepend(List L, int data)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    if (L != NULL)
+    {
+        if (L->length != 0)
+        {
+            Node temp = newNode(data);
+            L->front->prev = temp;
+            temp->next = L->front;
+            L->front = temp;
+            L->length++;
+            L->cursorIndex++;
+        }
+        else
+        {
+            Node temp = newNode(data);
+            L->front = L->back = temp;
+            L->length++;
+        }
+    }
+}
+void append(List L, int data)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    if (L != NULL)
+    {
+        if (L->length != 0)
+        {
+            Node temp = newNode(data);
+            L->back->next = temp;
+            temp->prev = L->back;
+            L->back = temp;
+            L->length++;
+        }
+        else
+        {
+            Node temp = newNode(data);
+            L->front = L->back = temp;
+            L->length++;
+        }
+    }
+}
+
+void insertBefore(List L, int data)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    if (L != NULL)
+    {
+        if (L->cursor == L->front) // Can append if theres only a single element in list.
+        {
+            prepend(L, data);
+        }
+        else
+        {
+            Node temp = newNode(data);
+            temp->next = L->cursor;
+            temp->prev = L->cursor->prev;
+            L->cursor->prev->next = temp;
+            L->cursor->prev = temp;
+            L->length++;
+            if (L->cursor != NULL)
+            {
+                L->cursorIndex++;
+            }
+        }
+    }
+}
+
+void insertAfter(List L, int data)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    if (L != NULL)
+    {
+        if (L->cursor == L->back) // Can append if theres only a single element in list.
+        {
+            append(L, data);
+        }
+        else
+        {
+            Node temp = newNode(data);
+            temp->prev = L->cursor;
+            temp->next = L->cursor->next;
+            L->cursor->next->prev = temp;
+            L->cursor->next = temp;
+            L->length++;
+            if (L->cursor != NULL)
+            {
+                L->cursorIndex++;
+            }
+        }
+    }
+}
+void deleteFront(List L)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    Node N= NULL;
+    if (L != NULL && L != 0)
+    {
+        if (L->cursor == L->front)
+        {
+            L->cursor = NULL;
+            L->cursorIndex = -1;
+        }
+        if (L->cursor != NULL)
+        {
+            L->cursorIndex--;
+        }
+        if(L->length > 1)
+        {
+            N = L->front; // Set Node to L->front for free function.
+            L->front = L->front->next;
+            freeNode(&N);
+         }
+        else
+        {
+            N = L->front; // Set Node to L->front for free function.
+            freeNode(&N);
+            L->front = NULL;
+        }
+        L->length--;
+    }
+}
+void deleteBack(List L)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    Node N = NULL;
+    if (L != NULL && L!= 0)
+    {
+        if (L->cursor == L->back)
+        {
+            L->cursor = NULL;
+            L->cursorIndex = -1;
+        }
+        if (L->length > 1)
+        {
+            N = L->back;  // Set Node to L->back for free function.
+            L->back = L->back->prev;
+            freeNode(&N);
+        }
+        else
+        {
+            N = L->back;  // Set Node to L->back for free function.
+            freeNode(&N);
+            L->back = NULL;
+        }
+        L->length--;
+    }
+}
+void delete(List L)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    Node N = NULL;
+    N = L->cursor;  // Set Node to L->cursor for free function.
+    if (L != NULL)
+    {
+        if (L->cursor == L->front)
+        {
+            deleteFront(L);
+        }
+        else if (L->cursor == L->back)
+        {
+            deleteBack(L);
+        }
+        else
+        {
+            L->cursor->prev->next = L->cursor->next;
+            L->cursor->next->prev = L->cursor->prev;
+            L->cursor = NULL;
+            L->cursorIndex = -1;
+            L->length--;
+            freeNode(&N);
+        }
+    }
+}
+// Other operations -----------------------------------------------------------
+void printList(FILE* out, List L)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    Node N = NULL;
+    if (L != NULL)
+    {
+        for (N = L->front; N != NULL; N = N->next)
+        {
+            fprintf(out,"%d ", N->data);
+        }
+        fprintf(out,"\n");
+    }
+}
+List copyList(List L)
+{
+    if(L == NULL)
+    {
+        printf("List Error: NULL\n");
+        exit(1);
+    }
+    List copiedList = newList();
+    Node temp = L->front;
+    while (temp != NULL)
+    {
+        append(copiedList, temp->data);
+        temp = temp->next;
+    }
+    copiedList->cursor = NULL;
+    copiedList->cursorIndex = -1;
+    return copiedList;
+}
